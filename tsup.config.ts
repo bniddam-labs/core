fc46c51b595ad215bc7612377d19935ba1dc41da
Benjamin NIDDAM <benjamin@weaverize.com>
2025-12-06 23:42:02 +0100

    Simplify build: single entry point, remove subpath exports - v0.2.0

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,11 +1,7 @@
 import { defineConfig } from 'tsup';
 
 export default defineConfig({
-  entry: {
-    index: 'src/index.ts',
-    'config/index': 'src/config/index.ts',
-    'logging/index': 'src/logging/index.ts',
-  },
+  entry: ['src/index.ts'],
   format: ['cjs', 'esm'],
   dts: true,
   sourcemap: true,
